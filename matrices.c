9c782a8d2c721de857adf555d8eb8965bcc3fd3d
JORGE ANDRES MADRID WOLFF <joanmawo@hotmaiil.com>
2013-02-21 13:43:35 -0500

    Segunda clase

--- matrices.c
+++ matrices.c
@@ -2,14 +2,18 @@
 int main(int argc, char **argv){
   char *filein;
   float *data;
+  float *posY;
+  
 
   int n_row = 0;
   int n_column = 0;
 
 
   datos = load_data(filein, &n_row, &n_column);
-}
 
+  posY = construirY(data);
+}
+//Función que carga los datos del archivo y los dispone en una matriz data 
 float * load_data( char *filein, int *nf, int *nc){
   float * data;
   FILE *file;
@@ -39,10 +43,21 @@ do{
       data[i*2] = dato1;
       data[i*2 + 1] = dato2;
 	}
-    }
+    
 
   return data;
 }
+//Funciòn que construye el vector con las posiciones en Y
+float * construirY(float *data, int *nf){
+
+  posY = malloc(n_lines*sizeof(float));
+  for(i = 0; i<n_lines; i++){
+    
+    posY[i] = data[2i]
+      
+      }
+    }
+
 
 
    
