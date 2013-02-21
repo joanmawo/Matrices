f258b042aa9b5c26fa53ea886c1a3cd1ccb222b3
JORGE ANDRES MADRID WOLFF <joanmawo@hotmaiil.com>
2013-02-21 14:18:48 -0500

    Segunda clase 2.0

--- matrices.c
+++ matrices.c
@@ -1,8 +1,14 @@
 
+#include <stdio.h>
+#include <gsl>
+
+
 int main(int argc, char **argv){
   char *filein;
   float *data;
   float *posY;
+  float *G;
+  float *GT;
   
 
   int n_row = 0;
@@ -11,7 +17,11 @@ int main(int argc, char **argv){
 
   datos = load_data(filein, &n_row, &n_column);
 
-  posY = construirY(data);
+  posY = construirY(data, &n_row);
+
+  G = construirG(data, &n_row);
+
+  GT = transpose(G, &n_row);
 }
 //Función que carga los datos del archivo y los dispone en una matriz data 
 float * load_data( char *filein, int *nf, int *nc){
@@ -47,19 +57,44 @@ do{
 
   return data;
 }
-//Funciòn que construye el vector con las posiciones en Y
+//Funciòn que construye el vector con las posiciones en Y a partir de la matriz de datos 'data'
 float * construirY(float *data, int *nf){
 
+  *nf = n_lines;
   posY = malloc(n_lines*sizeof(float));
   for(i = 0; i<n_lines; i++){
     
-    posY[i] = data[2i]
+    posY[i] = data[2i + 1]
       
       }
+
+  return posY;
     }
 
+//Función que construye la matriz G con "la fìsica del problema" a partir de la matriz de datos 'data'
+float * construirG(float *data, int *nf){
+  
+  n_lines = *nf;
+  
+  G = malloc(n_lines*3*sizeof(float));
 
+  for(i = 0; i<n_lines; i++){
+    
+    G[i*3] = 1
+    G[i*3 + 1] = data[2*i];
+    G[i*3 + 2] = (1/2)*(data[2*i])*(data[2*i]);
 
+      
+      }
+
+  return G;
+    }
+
+//Función que trasponer
+float * transpose(float *G, int *nf){
+
+  GT = malloc(n_lines*3*sizeof(float));
+}
    
 //organized_data =  organize_data(datos, nfilas, ncolumnas)
 
