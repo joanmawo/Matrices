4c9fd911a3f6b34f79bed7b2d2719691e5dc2bc2
JORGE ANDRES MADRID WOLFF <joanmawo@hotmail.com>
2013-03-04 16:45:49 -0500

    1.0

--- pca.c
+++ pca.c
@@ -11,6 +11,7 @@ int main(int argc, char ** argv){
   gsl_matrix *covarianzas;
   gsl_matrix *eigenVec;
   gsl_vector *eigenVal;
+  gsl_vector *Vgsl;
 
   columnas = 3;
   lineas = count_lines(argv[1]);
@@ -19,7 +20,22 @@ int main(int argc, char ** argv){
   covarianzas = gsl_matrix_calloc(columnas, columnas);
   eigenVec = gsl_matrix_calloc(columnas, columnas);
   eigenVal = gsl_vector_calloc(columnas);
+  Vgsl = gsl_vector_calloc(lineas);
+
+  //Funcion que calcula promedios de componentes de vectores
+  double calcularPromedio(gsl_vector *Vgsl, gsl_matrix *datos){
+
+    double temp = 0.0;
+    int j;
+    for(j = 0; j < lineas; j ++){
+      gsl_matrix_get_col(Vgsl, datos, j);
+    }
+    for(j = 0; j < lineas; j ++){
+      temp += gsl_vector_get(Vgsl, j);
+    }
+   
+    return = (temp/lineas);
+  }
 
  
-
 }
